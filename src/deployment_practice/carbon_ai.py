3b66417e168246ff96c2437c5d9dad141f18cd49
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-06-30 17:32:08 -0600

    updates

--- src/deployment_practice/carbon_ai.py
+++ src/deployment_practice/carbon_ai.py
@@ -1,30 +1,42 @@
 import boto3
 import os
-import sklearn
-from joblib import Parallel, delayed
+from .database_tools import DB
+from sklearn.linear_model import LinearRegression
 
 
 class CarbonAI:
-    def __init__(self, input_folder_path: str):
-        self.input_folder_path = input_folder_path
+    def __init__(self):
+        # get environment variables
+        self.model_output_path = os.get_environ("MODEL_PATH")
+        # set up aws resources
         self.bucket_name = os.get_environ("S3_BUCKET")
         self.s3 = boto3.resource("s3")
         self.bucket = self.s3.Bucket(self.bucket_name)
         self.s3_client = self.s3.client()
-    def train(self):
-        return NotImplementedError
-    def predict(self):
-        return NotImplementedError
-    def download_data(self):
-        """TODO - hopefully multipart isn't needed - if so, switch to bucket.download_fileobj"""
-        #get keys of object in folder
-        response = self.s3_client.list_objects(Bucket=self.bucket_name, prefix = self.input_folder_path)
-        object_keys = [response["Contents"][i]["Key"] for i in len(response["Contents"])]
-        #helper function for parallelization
-        def _download_file(file_key:str):
-            self.bucket.download_file(file_key, file_key)
-        # downloads files in parallel cpu jobs    
-        Parallel(n_jobs=-1)(delayed(_download_file)(file_key) for file_key in object_keys)
-    def preprocess(self):
+        self.db = DB()
+        self.input_df = self.db.db_download_data('input_data')
+        self.train_df = None
+        self.time_steps = 5
+        self.model = None
+        self.set_model()
+    def set_model(self):
+        # TODO - lots
+        self.model = LinearRegression(n_jobs=-1)
+
+    def preprocess(self, df: pd.DataFrame):
+        self.train_df = self.input_df.drop(axis='columns', labels=["start_time", "end_time", "intensity_index"])
         return NotImplementedError
 
+    def train(self):
+        self.preprocess()
+        #split into time series 'x_data' and next step labels
+        x_data = [self.train_df.iloc[[i-self.time_steps:i]].values for i in range(self.time_steps, train_df.shape[0]-1)]
+        labels = [self.train_df["actual"].iloc[[i]].values for i in range(self.time_steps, self.train_df.shape[0])]
+        # train model
+        self.model = self.model.fit(X=x_data,y=labels)
+        self.save_model()
+    def save_model():
+        return NotImplementedError
+    def predict(self):
+        self.preprocess()
+        return NotImplementedError
--- src/deployment_practice/data_pull.py
+++ src/deployment_practice/data_pull.py
@@ -1,12 +1,61 @@
-import boto3
-import os
+import requests
+from joblib import Parallel, delayed
+import pandas as pd
+import datetime
+from .database_tools import DB
+
+
+
 class DataPull:
-    def __init__(self):
-        self.data_url = 'https://carbonintensity.org.uk/'
-        self.s3 = boto3.resource('s3')
-        self.bucket_name = os.get_environ('S3_BUCKET')
-        self.bucket = self.s3.Bucket(self.bucket_name)
-    def pull_data(self):
-        
-    def s3_upload_data(self):
+    def __init__(self, datetime_range: pd.date_range = None):
+        #set up variables for data pull and processing
+        self.data_url = f'https://api.carbonintensity.org.uk/intensity/date/' # to add {from} Datetime in in ISO8601 format
+        self.responses = None
+        self.df = None
+        self.table_name = 'input_data'
+        # creates pandas satetime range for timesteps to query
+        self.datetime_range = self.set_datetime_range() if datetime_range is None else datetime_range # ISO8601 format
+        self.db = DB()
+
+
+    def main(self):
+        """Downloads data uploads it to mysql database as os.environ("DB_HOST"). Needs __init__ to be run first"""
+        self.download_data()
+        self.process_responses()
+        self.db.db_upload_data(df=self.df, table_name=self.table_name)
+
+    def set_datetime_range(self):
+        self.datetime_range = pd.date_range(end=datetime.now(), periods=500, freq="30min")
+
+    def download_data(self):
+        datetimes = self.date_time_range.dt.strfrmt("%Y-%m-%dT%H:%minZ").values
+        headers = {
+            'Accept': 'application/json'
+            }
+        # requests the intensity data from datetimes in parallel
+        self.responses = Parallel(n_jobs=-1)(delayed(requests.get)(f'{self.data_url}{datetime}', headers=headers)  for datetime in datetimes)
+
+    def process_responses(self):
+        self._data = list()
+        def _process_response(response):
+            # if response["error"] exists:
+            #     raise Exception(f'{response["error"]}')
+            #pull data from responses
+            data_dict = {}
+            response_data = response["data"]
+            data_dict["start_time"] = response_data["from"] 
+            data_dict["end_time"] = response_data["to"]
+            data_dict["intensity_forecast"] = response_data["intensity"]["forecast"]
+            data_dict["intensity_actual"] = response_data["intensity"]["actual"]
+            data_dict["intensity_index"] = response_data["intensity"]["index"]
+            # add dictionary of response data to data list for df use
+            self._data.append(data_dict)
+        # process responses in parallel
+        Parallel(n_jobs=-1)(delayed(_process_response)(response) for response in self.responses)
+        #create dataframe out of data
+        self.df = pd.df(self._data)
+
+if __name__ == "__main__":
+    data_pull = DataPull()
+    data_pull.main()
         
