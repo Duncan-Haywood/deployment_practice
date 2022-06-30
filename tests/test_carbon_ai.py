12cfc6edeaeafa312c095936c9c935ca58a27f88
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-06-30 17:31:15 -0600

    initial tests

---
+++ tests/test_carbon_ai.py
@@ -0,0 +1,6 @@
+from deployment_practice import carbon_ai
+from deployment_practice.carbon_ai import CarbonAI
+def test_main():
+    carbon_ai =- CarbonAI()
+    carbon_ai.main()
+    #TODO check results
---
+++ tests/test_data_pull.py
@@ -0,0 +1,6 @@
+from deployment_practice import data_pull
+from deployment_practice.data_pull import DataPull
+def test_main():
+    data_pull = DataPull()
+    data_pull.main()
+    # TODO check results
