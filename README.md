35ec5ccd973ad1b630f4c0ba445700c838dc07d7
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-07-01 13:54:11 -0600

    init environment variable file todo

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # deployment_practice
 interview for Habitat
+## Run instructions:
+data pull service:
+`docker build `
+predict service:
+``
+## Development plan:
+- Unfortunately, I have other projects to juggle at the moment, and I can't finish all I'd like to do with this. 
+- A lot of this would be for a more important/larger project with high development time spent. Some of this would be cut for something that is this simple. 
+- Next steps include:
+- Terraform setup for all infrastructure. 
+- aws eventbridge and lambda container setup for data pull service on time schedule.
+- Testing for code
+- documenting usage etc. 
+- Setting test and production env variables for local(dev)/cloud(prod/testing) databases and object stores
+- MLFlow server setup for future experimentation.  
+- aws api gateway, fastAPI setup on lambda container for calling prediction task. fast API Spins up ec2 for prediction running for the smaller linear regression model . 
+- vpc, networking etc. setup
+- docker image repository setup on aws ecr. 
+- github actions or equivalent setup for builds and tests and upload of images on push/merge to main.
+- Possibly feature store setup. 
--- src/deployment_practice/carbon_ai.py
+++ src/deployment_practice/carbon_ai.py
@@ -8,35 +8,52 @@ class CarbonAI:
     def __init__(self):
         # get environment variables
         self.model_output_path = os.get_environ("MODEL_PATH")
-        # set up aws resources
-        self.bucket_name = os.get_environ("S3_BUCKET")
-        self.s3 = boto3.resource("s3")
-        self.bucket = self.s3.Bucket(self.bucket_name)
-        self.s3_client = self.s3.client()
+        # # set up aws resources -TODO for handling artifacts
+        # self.bucket_name = os.get_environ("S3_BUCKET")
+        # self.s3 = boto3.resource("s3")
+        # self.bucket = self.s3.Bucket(self.bucket_name)
+        # self.s3_client = self.s3.client()
         self.db = DB()
         self.input_df = self.db.db_download_data('input_data')
         self.train_df = None
         self.time_steps = 5
         self.model = None
         self.set_model()
+
+    def main(self):
+        self.train()
+        self.predict()
     def set_model(self):
-        # TODO - lots
+        # TODO - checking whether a model exists already etc. and loading it
         self.model = LinearRegression(n_jobs=-1)
 
     def preprocess(self, df: pd.DataFrame):
-        self.train_df = self.input_df.drop(axis='columns', labels=["start_time", "end_time", "intensity_index"])
-        return NotImplementedError
+        # TODO error handling for double pd.df.drop call
+        self.clean_df = self.input_df.drop(axis='columns', labels=["start_time", "end_time", "intensity_index"]) 
 
     def train(self):
         self.preprocess()
         #split into time series 'x_data' and next step labels
-        x_data = [self.train_df.iloc[[i-self.time_steps:i]].values for i in range(self.time_steps, train_df.shape[0]-1)]
+        x_data = [self.clean_df.iloc[[i-self.time_steps:i]].values for i in range(self.time_steps, train_df.shape[0]-1)]
         labels = [self.train_df["actual"].iloc[[i]].values for i in range(self.time_steps, self.train_df.shape[0])]
         # train model
         self.model = self.model.fit(X=x_data,y=labels)
         self.save_model()
-    def save_model():
+
+    def save_model(self):
         return NotImplementedError
+
+    def load_model(self):
+        return NotImplementedError
+
     def predict(self):
+        """predicts the next time step of "actual" intensity from latest datapull."""
         self.preprocess()
-        return NotImplementedError
+        df_length = self.clean_df.shape[0]
+        x_data = self.clean_df.iloc[[df_length-self.time_steps: df_length]]
+        results = self.model.predict(x_data)
+        return results # TODO - better storage functionality
+
+if __name__=='__main__':
+    carbonai = CarbonAI()
+    carbonai.main()
